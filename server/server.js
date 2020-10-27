8fff063f2f0bca935e061a44ff7091a69d1defb3
petrosly <petroslymper@gmail.com>
2020-10-27 19:11:49 +0200

    Added User model inside models folder

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const logger = require("morgan");
 const path = require("path");
 const mongoose = require("mongoose");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 const database = "mongodb://localhost:27017/test";
 
@@ -19,13 +18,3 @@ mongoose
   .catch((error) => {
     console.error(error);
   });
-
-app.get("/", (req, res) => {
-  res.send("Trexeis!");
-});
-
-const user = new mongoose.Schema({
-  name: String,
-});
-
-const User = mongoose.model("user", user);
